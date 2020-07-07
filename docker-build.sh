7a31617245a4dc865de7707ea4dc487f3f81ba23
Gavin Rees <gavinrees@college.harvard.edu>
2020-07-07 11:36:44 -0400

    Updated readme and bugfix
    Changed docker-build.sh to accept an argument that is the version of the container to build in, updated Readme

--- docker-build.sh
+++ docker-build.sh
@@ -2,6 +2,6 @@
 # function rel_to_abs_path() {
 #     cd $1 && pwd
 # }
-./build-docker-image.sh . 20
+./build-docker-image.sh . $1
 # docker run -it --mount type=bind,source=$(rel_to_abs_path $1 ),destination=/FFmpeg ffmpeg-builder-ubuntu18 
 # docker run -it ffmpeg-builder-ubuntu18 /bin/bash
