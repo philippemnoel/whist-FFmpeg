ceff7e31a02a567267f6f3688b40d51a12a20686
Gavin Rees <gavinrees@college.harvard.edu>
2020-07-07 09:46:30 -0400

    Ubuntu 18 working builder container
    Working ubuntu 18 container that builds the FFmpeg repo and installs it within itself during build process (installs in image, not as running container)

---
+++ docker-build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+function rel_to_abs_path() {
+    cd $1 && pwd
+}
+./build-docker-image.sh .
+# docker run -it --mount type=bind,source=$(rel_to_abs_path $1 ),destination=/FFmpeg ffmpeg-builder-ubuntu18 
+# docker run -it ffmpeg-builder-ubuntu18 /bin/bash
