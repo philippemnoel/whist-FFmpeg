7c4687e8ac6802ec1ac4fb5e34ce50dc1a13c5a7
Roshan Padaki <roshan@whist.com>
2021-11-30 19:35:43 -0500

    Add Docker build system and CI

---
+++ docker-build-scripts/build_ffmpeg.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Configure and Build FFmpeg on Linux Ubuntu with relevant flags to build shared libs
+
+cd FFmpeg
+./configure \
+--arch=x86_64 \
+--extra-cflags=-O3 \
+--enable-gpl \
+--enable-nonfree \
+--enable-version3 \
+--disable-programs \
+--disable-doc \
+--disable-debug \
+--disable-sdl2 \
+--enable-opengl \
+--enable-frei0r \
+--enable-libfdk-aac \
+--enable-libopus \
+--enable-libx264 \
+--enable-libx265 \
+--enable-cuda-nvcc \
+--enable-nvenc \
+--enable-nvdec \
+--enable-libmfx \
+--enable-filter=frei0r \
+--enable-filter=scale_cuda \
+--disable-static \
+--enable-shared
+
+# Build FFmpeg and move static/shared libs
+make -j && rm -rf linux-build && mkdir linux-build
+find ./ -type f \( -iname \*.so -o -iname \*.a \) | xargs -I % cp % linux-build/ 
+make install && ldconfig
