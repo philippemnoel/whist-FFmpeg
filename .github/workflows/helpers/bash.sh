bbc96545fcafd2a41ee03f0f09525ecda0f4a478
Philippe Noël <21990816+philippemnoel@users.noreply.github.com>
2022-08-29 13:30:48 -0400

    readd
    Signed-off-by: Philippe Noël <21990816+philippemnoel@users.noreply.github.com>

--- .github/workflows/helpers/bash.sh
+++ .github/workflows/helpers/bash.sh
@@ -3,6 +3,6 @@
 logFile=$(cygpath -u "$1" 2> /dev/null)
 command=$2
 shift 2
-[[ -z $logFile || -z $command ]]
+[[ -z $logFile || -z $command ]] && exit 1
 script -eqf --command "/usr/bin/bash -o pipefail -lc '$command $*'" /dev/null | tee "$logFile"
 exit "${PIPESTATUS[0]}"
